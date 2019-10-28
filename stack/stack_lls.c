c971c5e80949813c1499a9018336b19e1f5a7ff4
nullbyte91 <nullbyte.in@gmail.com>
2019-10-28 16:51:05 +0530

    stack lls optimiation and pointer fix

--- stack/stack_lls.c
+++ stack/stack_lls.c
@@ -4,57 +4,55 @@
 
 #define EMPTY_STACK 0
 
-typedef struct Stack{
+struct Stack{
     int val;
     struct Stack *next;
-}*Stack;
+};
 
 void 
-push(Stack *s, int val){
-    Stack stack_node = malloc(sizeof(Stack));
+push(struct Stack **s, int val){
+    struct Stack* stack_node = malloc(sizeof(struct Stack));
     stack_node->val = val;
     stack_node->next = *s;
     *s = stack_node;
 }
 
 void
-printStack(Stack* s){
-    Stack e = *s;
-    while (e != NULL){
-        printf("%d ", e->val);
-        e = e->next;
+printStack(const struct Stack* s){
+    while (s != NULL){
+        printf("%d ", s->val);
+        s = s->next;
     }
     putchar('\n');
 }
 
 void 
-pop(Stack* s){
-    Stack e = *s;
+pop(struct Stack** s){
+    struct Stack* e = *s;
     printf("Pop: %d \n", e->val);
     *s = e->next;
     free(e);
 }
 
 int
-peek(Stack* s){
-    Stack e = *s;
-    return e->val;
+peek(const struct Stack* s){
+    return s->val;
 }
 
 bool
-isEmpty(Stack* s){
-
+isEmpty(const struct Stack* s){
+    return s == NULL;
 }
 
 int
 main(int argc,char** argcv){
-    Stack s = EMPTY_STACK;
+    struct Stack* s = EMPTY_STACK;
 
     for (int i=0; i<5; i++){
         push(&s, i);
     }
-    printStack(&s);
-    printf("Peek: %d\n", peek(&s));
+    printStack(s);
+    printf("Peek: %d\n", peek(s));
     for (int i=0; i<5; i++){
         pop(&s);
     }
